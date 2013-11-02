e339dde4eadbefbc7feb2daab5fcf1c17fe2a2a4
Nikhil Taneja <nikhil.a.taneja@gmail.com>
2013-11-01 19:03:03 -0600

    add delete feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,25 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+
+    redirect_to article_path(@article)
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+
 end
